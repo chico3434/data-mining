36163e375ca3a8f194d1e5cbb7b3ce7751dd0849
chico3434 <franciscok.doamaral@gmail.com>
2025-07-02 22:17:04 -0300

    Melhorando análise

--- analysis.R
+++ analysis.R
@@ -2,3 +2,11 @@
 load("bfd_2022.rdata")
 
 summary(bfd)
+
+# nota-se um grande número (mais de 10% nna maioria das variáveis) de NA's nas variáveis relacionadas a tempo ou condições do voo (temperatura, umidade, vento, pressão, visibilidade).
+# a variável "observação" possui apenas NA's
+
+# companhia aérea
+table(bfd$company)
+
+
